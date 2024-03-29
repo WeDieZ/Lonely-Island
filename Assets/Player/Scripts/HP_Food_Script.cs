b0e1e6c8b675c4b3025ff19bfffa3a018050c1b3
WeDieZ <wediez09@gmail.com>
2024-03-29 22:27:05 +0700

    Initial commit

---
+++ Assets/Player/Scripts/HP_Food_Script.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class HP_Food_Script : MonoBehaviour
+{
+    public float food = 100.0f;
+    public float hp = 100;
+    private float _maxHP;
+    private float _maxFood;
+    public RectTransform valueRectTransformHP;
+    public RectTransform valueRectTransformFood;
+
+
+    void Start()
+    {
+        _maxHP = hp;
+        DrawHP();
+        _maxFood = food;
+        DrawFood();
+    }
+
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            OnDead();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Hunger();
+
+        if (food <= 0)
+        {
+            DealDamage_Hunger();
+        }
+    }
+
+    //decoding_
+
+    public void DrawHP()
+    {
+        valueRectTransformHP.anchorMax = new Vector2(hp / _maxHP, 1);
+    }
+
+    public void DealDamage()
+    {
+        hp -= 1 * Time.deltaTime;
+        DrawHP();
+    }
+
+    public void OnDead()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void DrawFood()
+    {
+        valueRectTransformFood.anchorMax = new Vector2(food / _maxFood, 1);
+    }
+
+    public void Hunger()
+    {
+        food -= 0.05f;
+        DrawFood();
+    }
+
+    public void DealDamage_Hunger()
+    {
+        hp -= 1 * Time.fixedDeltaTime;
+        DrawHP();
+    }
+
+    //_decoding
+}
---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 5;
+    private float jumpforce = 5.5f;
+    private float gravity = 9.8f;
+    private float _fallVelocity = 0;
+    private int can_jump = 1;
+
+    public Animator _animator;
+    private CharacterController _characterController;
+    private Vector3 _VectorMove;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        Fall();
+
+        Move();
+    }
+
+    void Update()
+    {
+        Jump();
+
+        Walk_Move();
+    }
+
+    //decoding_
+
+    private void Walk_Move()
+    {
+        _VectorMove = Vector3.zero;
+        var run_direction = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _VectorMove += transform.forward;
+            run_direction = 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _VectorMove -= transform.forward;
+            run_direction = 2;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _VectorMove += transform.right;
+            run_direction = 3;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _VectorMove -= transform.right;
+            run_direction = 4;
+        }
+
+        _animator.SetInteger("run_direction", run_direction);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && can_jump > 0)
+        {
+            can_jump -= 1;
+            _fallVelocity = -jumpforce;
+        }
+    }
+
+    private void Fall()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+            can_jump = 1;
+        }
+    }
+
+    private void Move()
+    {
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        _characterController.Move(_VectorMove * speed * Time.fixedDeltaTime);
+    }
+
+    //_decoding
+}
