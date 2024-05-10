1c935d93b795582aac7935ba45b2ecad6da550e9
WeDieZ <wediez09@gmail.com>
2024-05-10 20:42:55 +0700

    Add_Animals

---
+++ Assets/Lesha_R/Enemy/Peaceful.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.AI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peaceful : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+
+    private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        NewPatrol();
+    }
+
+    void Update()
+    {
+        NewPatrolUpdate();
+    }
+
+    //decoding_
+
+    private void NewPatrol()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+    private void NewPatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+        {
+            _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+        }
+    }
+
+    //_decoding
+}
